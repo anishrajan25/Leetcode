db54db9e5f0d297dd5c9e39ffaafc25113d12ce2
Anish Rajan <anishrajan2708@gmail.com>
2020-09-26 15:00:37 +0530

    longest word in dictionary

---
+++ 720 Longest Word in Dictionary.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/longest-word-in-dictionary/
+
+
+class Solution {
+    public String longestWord(String[] words) {
+        Set<String> wordset = new HashSet();
+        for (String word: words) wordset.add(word);
+        Arrays.sort(words, (a, b) -> a.length() == b.length()
+                    ? a.compareTo(b) : b.length() - a.length());
+        for (String word: words) {
+            boolean good = true;
+            for (int k = 1; k < word.length(); ++k) {
+                if (!wordset.contains(word.substring(0, k))) {
+                    good = false;
+                    break;
+                }
+            }
+            if (good) return word;
+        }
+
+        return "";
+    }
+}
