5ffc2c7abfffac42073d5abc0b3f6a75ede88b74
Anish Rajan <anishrajan2708@gmail.com>
2020-10-14 19:55:32 +0530

    Sum of Square Numbers

---
+++ 633 Sum of Square Numbers.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/sum-of-square-numbers/
+
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        double sqr = Math.sqrt(c);
+        if(sqr == (int)sqr) return true;
+        for(int i = 0; i < sqr; i++) {
+            double x = Math.sqrt(c - i*i);
+            if(x == (int)x) return true;
+        }
+        return false;
+    }
+}
