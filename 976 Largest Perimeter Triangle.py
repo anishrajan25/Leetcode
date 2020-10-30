499e64644eba7d5c48750b6f99ac2c2b7e3042b1
Anish Rajan <anishrajan2708@gmail.com>
2020-10-30 23:51:01 +0530

    Largest Perimeter Triangle

---
+++ 976 Largest Perimeter Triangle.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/largest-perimeter-triangle/
+
+class Solution(object):
+    def largestPerimeter(self, A):
+        A.sort()
+        for i in xrange(len(A) - 3, -1, -1):
+            if A[i] + A[i+1] > A[i+2]:
+                return A[i] + A[i+1] + A[i+2]
+        return 0
