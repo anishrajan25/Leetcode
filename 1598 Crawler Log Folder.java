a4fc914eee6a818d2a644fd9e3b3f40b4efc6cdb
Anish Rajan <anishrajan2708@gmail.com>
2020-11-08 13:53:14 +0530

    Crawler Log Folder

---
+++ 1598 Crawler Log Folder.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/crawler-log-folder/
+
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> dir = new Stack();
+        for(String op: logs) {
+            if(op.equals("../")) {if(!dir.isEmpty()) dir.pop();}
+            else if(op.equals("./")) continue;
+            else dir.push(op);
+        }
+        
+        return dir.size();
+    }
+}
