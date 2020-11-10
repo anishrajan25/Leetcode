da79e9aff6f0d9fdea6cde40c7462b3a6ccb5930
Anish Rajan <anishrajan2708@gmail.com>
2020-11-10 13:59:09 +0530

    Divide Array in Sets of K Consecutive Numbers

---
+++ 1296 Divide Array in Sets of K Consecutive Numbers.java
@@ -0,0 +1,25 @@
+//  https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/
+
+
+class Solution {
+    public boolean isPossibleDivide(int[] hand, int W) {
+         if(W==0 || hand.length%W!=0) return false;
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        Arrays.sort(hand);
+        for (int x : hand)
+            hm.put(x, hm.getOrDefault(x, 0) + 1);
+        for (int x : hand)
+        {
+            if (hm.get(x) == 0)
+                continue;
+            hm.put(x, hm.get(x) - 1);
+            for (int i = 1; i < W; i++)
+            {
+                if (hm.getOrDefault(x + i, 0) == 0)
+                    return false;
+                hm.put(x + i, hm.get(x + i) - 1);
+            }
+        }
+        return true;
+    }
+}
