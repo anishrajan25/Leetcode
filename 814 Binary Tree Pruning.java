fe4888bb3572b4fcb5f8f7fb99a0962e30cebb14
Anish Rajan <anishrajan2708@gmail.com>
2020-11-28 13:43:43 +0530

    Binary Tree Pruning

---
+++ 814 Binary Tree Pruning.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/binary-tree-pruning/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if(!helper(root)) return null;
+        return root;
+    }
+    
+    private boolean helper(TreeNode root) {
+        if(root == null) return false;
+        
+        boolean left = helper(root.left), right = helper(root.right);
+        if(!left) root.left = null;
+        if(!right) root.right = null;
+        
+        return root.val == 1 || left || right;
+    }
+}
