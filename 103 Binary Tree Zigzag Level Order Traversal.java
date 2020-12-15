d2500f55d6a4155b5458c9284a5d0e5f6fda9b01
Anish Rajan <anishrajan2708@gmail.com>
2020-12-15 15:51:49 +0530

    updated Binary Tree Zigzag Level Order Traversal

--- 103 Binary Tree Zigzag Level Order Traversal.java
+++ 103 Binary Tree Zigzag Level Order Traversal.java
@@ -17,36 +17,70 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        if(root == null) return new LinkedList<List<Integer>>();
-        LinkedList<List<Integer>> res = new LinkedList<List<Integer>>();
-        LinkedList<TreeNode> itr = new LinkedList();
-        LinkedList<Integer> store = new LinkedList<Integer>();
-        boolean right = true;
+        List<List<Integer>> res = new ArrayList();
+        if(root == null) return res;
         
-        itr.add(null);
-        itr.addFirst(root);
-        while(itr.size() > 1) {
-            TreeNode tmp = null;
+        LinkedList<TreeNode> l = new LinkedList<TreeNode>();
+        l.add(root);
+        boolean leftToRight = true;
+        while(!l.isEmpty()) {
+            List<Integer> tmp = new ArrayList();
+            LinkedList<TreeNode> next = new LinkedList<TreeNode>();
             
-            if(right) {
-                tmp = itr.pollFirst();
-                if(tmp.left != null) itr.addLast(tmp.left);
-                if(tmp.right != null) itr.addLast(tmp.right);
+            while(!l.isEmpty()) {
+                TreeNode t = l.removeFirst();
+                tmp.add(t.val);
+                if(leftToRight) {
+                    if(t.left != null) next.addFirst(t.left);
+                    if(t.right != null) next.addFirst(t.right);
+                }
+                else {
+                    if(t.right != null) next.addFirst(t.right);
+                    if(t.left != null) next.addFirst(t.left);
+                }
             }
-            else {
-                tmp = itr.pollLast();
-                if(tmp.right != null) itr.addFirst(tmp.right);
-                if(tmp.left != null) itr.addFirst(tmp.left);
-            }
-            
-            store.add(tmp.val);
-            
-            if((right && itr.peekFirst() == null) || (!right && itr.peekLast() == null)) {
-                res.addLast(store);
-                store = new LinkedList();
-                right = !right;
-            }
+            leftToRight = !leftToRight;
+            res.add(tmp);
+            l = next;
         }
+        
         return res;
     }
 }
+
+
+// class Solution {
+//     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+//         if(root == null) return new LinkedList<List<Integer>>();
+//         LinkedList<List<Integer>> res = new LinkedList<List<Integer>>();
+//         LinkedList<TreeNode> itr = new LinkedList();
+//         LinkedList<Integer> store = new LinkedList<Integer>();
+//         boolean right = true;
+        
+//         itr.add(null);
+//         itr.addFirst(root);
+//         while(itr.size() > 1) {
+//             TreeNode tmp = null;
+            
+//             if(right) {
+//                 tmp = itr.pollFirst();
+//                 if(tmp.left != null) itr.addLast(tmp.left);
+//                 if(tmp.right != null) itr.addLast(tmp.right);
+//             }
+//             else {
+//                 tmp = itr.pollLast();
+//                 if(tmp.right != null) itr.addFirst(tmp.right);
+//                 if(tmp.left != null) itr.addFirst(tmp.left);
+//             }
+            
+//             store.add(tmp.val);
+            
+//             if((right && itr.peekFirst() == null) || (!right && itr.peekLast() == null)) {
+//                 res.addLast(store);
+//                 store = new LinkedList();
+//                 right = !right;
+//             }
+//         }
+//         return res;
+//     }
+// }
