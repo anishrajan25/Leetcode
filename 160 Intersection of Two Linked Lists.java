95be01bd1a7707a932b5f9505ec7dd0d75ce003c
Anish Rajan <anishrajan2708@gmail.com>
2020-12-27 16:14:46 +0530

    Intersection of Two Linked Lists

--- 160 Intersection of Two Linked Lists.java
+++ 160 Intersection of Two Linked Lists.java
@@ -11,13 +11,53 @@
  *     }
  * }
  */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode pa = headA, pb = headB;
-        while (pa != pb) {
-            pa = (pa != null) ? pa.next : headB;
-            pb = (pb != null) ? pb.next : headA;
+        Stack<ListNode> a = new Stack(), b = new Stack();
+        
+        ListNode tmp = headA;
+        while(tmp != null) {
+            a.push(tmp);
+            tmp = tmp.next;
         }
-        return pa;
+        tmp = headB;
+        while(tmp != null) {
+            b.push(tmp);
+            tmp = tmp.next;
+        }
+        
+        while(!a.isEmpty() && !b.isEmpty()) {
+            if(a.peek() == b.peek()) {
+                tmp = a.pop();
+                b.pop();
+            }
+            else break;
+        }
+        
+        return tmp;
     }
 }
+
+
+// public class Solution {
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         ListNode pa = headA, pb = headB;
+//         while (pa != pb) {
+//             pa = (pa != null) ? pa.next : headB;
+//             pb = (pb != null) ? pb.next : headA;
+//         }
+//         return pa;
+//     }
+// }
