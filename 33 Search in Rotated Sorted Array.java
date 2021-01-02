64112c1c1cceba334e32f2109cd7c4ae6b19847f
Anish Rajan <anishrajan2708@gmail.com>
2021-01-02 16:18:36 +0530

    Search in Rotated Sorted Array

---
+++ 33 Search in Rotated Sorted Array.java
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+class Solution {
+    public int search(int[] A, int target) {
+        for(int i = 0; i < A.length; ++i) if(A[i] == target) return i;
+        
+        return -1;
+    }
+}
