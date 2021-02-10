e13d9fbc6dcfdc0c195657c0fc96dd2880b30dad
anishrajan25 <anishrajan2708@gmail.com>
2021-02-10 11:43:28 +0530

    Maximum Gap

---
+++ 164 Maximum Gap.java
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/maximum-gap/
+
+class Solution {
+    public int maximumGap(int[] nums) {
+        Arrays.sort(nums);
+        int res = 0;
+        for(int i = 0; i < nums.length - 1; i++) res = Math.max(res, nums[i + 1] - nums[i]);
+        return res;
+    }
+}
