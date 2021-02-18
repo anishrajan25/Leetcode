5cbb0bc1f0b5c47f80a264d75ef13a2f14bdd66f
anishrajan25 <anishrajan2708@gmail.com>
2021-02-18 16:41:28 +0530

    add new solution arithmetic slices

--- 413 Arithmetic Slices.java
+++ 413 Arithmetic Slices.java
@@ -1,5 +1,23 @@
 // https://leetcode.com/problems/arithmetic-slices/
 
+
+class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+        if(A.length < 3) return 0;
+        int ans = 0, i = 0, j = 0;
+        
+        while(i < A.length - 1) {
+            int d = A[i + 1] - A[i];
+            j = i++;
+            while(i < A.length - 1 && A[i + 1] - A[i] == d) i++;
+            int len = i - j + 1 - 2;
+            if(len > -1) ans += len * (len + 1) / 2;
+        }
+        return ans;
+    }
+}
+
+// SOLUTION II
 class Solution {
     public int numberOfArithmeticSlices(int[] A) {
         if(A.length < 3) return 0;
